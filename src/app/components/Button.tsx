65875a456d1ba861dfa35a730ff8840f2c147bca
Aurore Dimech <aurore.dimech@edu.devinci.fr>
2025-01-17 10:23:29 +0100

    ADD custom main page with Button Component

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -6,7 +6,7 @@ type ButtonProps = {
 }
 
 const Button: React.FC<ButtonProps> = ({label, onClick}) => {
-    return <button onClick={onClick}>{label}</button>
+    return <button onClick={onClick} className="bg-black text-white p-2 rounded-md">{label}</button>
 }
 
 export default Button;
